8c0c35313927ddeb1ad69d940440d45a75b39abd
Eugene <evgeniy.boytsov@gmail.com>
2019-10-09 18:58:29 +0300

    created  remindme and myreminders commands

--- bot/commands.js
+++ bot/commands.js
@@ -1,6 +1,10 @@
+const nanoid = require('nanoid');
+
 const commads = {
     showteam: 'showteam - shows registered chat members',
     registerme: 'registerme - registers you to team members',
+    remindme: 'remindme - set a reminder. Example: /remindme breakfast 11:00',
+    myreminders: 'myreminders - show list of incomplited reminders',
     help: 'help - shows a list of available commands'
 };
 
@@ -40,23 +44,54 @@ const registerCommands = bot => {
     bot.command('remindme', ctx => {
         const { session, message, from, reply } = ctx;
 
-        const reminders = session.reminders || [];
+        session.reminders = session.remiders || [];
 
-        let reminder = reminders.find(r => r.name === message.text);
+        let reminder = session.reminders.find(r => r.name === message.text);
 
-        if (!reminder) {
-            const [ , name, time ] = message.text.split(' ');
+        let dateTimeString = '00:00';
+        if (reminder) {
+            dateTimeString = reminder.time;
+        } else {
+            const [, name, time] = message.text.split(' ');
 
-            reminder = {
-                name,
-                time
-            };
+            if (!name || !time) {
+                return reply(`@${from.username} 🤔 \n Hmm...I think it is better to specify message and time for your reminder /help`);
+            }
+
+            const nowTime = new Date().toString().match(/(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]/)[0];
+
+            if (nowTime > time) {
+                return reply(`
+                    @${from.username} 🤔 \n Hmm...To be honest I can't remind you in the past \n ...But you know about Terminator films 😇 /help
+                `);
+            }
+
+            dateTimeString = new Date().toString().replace(/(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]/, time);
+
+            reminder = { id: nanoid(), name, time };
+
+            session.reminders.push(reminder);
         }
 
-        reminders.push(reminder);
-        session.reminders = reminders;
+        const intervalId = setInterval(() => {
+            const timeNow = new Date();
+            const reminderTime = new Date(dateTimeString);
 
-        return reply(`${from.first_name || from.username} I remind you to "${reminder.name}" at ${reminder.time}`);
+            if (timeNow.getTime() >= reminderTime.getTime()) {
+
+                clearInterval(intervalId);
+
+                const reminderToExecute = session.reminders.find(r => r.id === reminder.id);
+
+                if (reminderToExecute) {
+                    session.reminders = session.reminders.filter(r => r.id !== reminder.id);
+
+                    return reply(`@${from.username} Time to ${reminder.name}`);
+                }
+            }
+        }, 5000);
+
+        return reply(`${from.first_name || from.username} I will remind you to "${reminder.name}" at ${reminder.time}`);
     });
 
     bot.command('myreminders', ctx => {
